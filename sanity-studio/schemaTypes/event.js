62d8c349cc76183a713ca26db818878eed1611ae
Brad Heath <bradheath96@gmail.com>
2024-12-18 14:48:39 +0000

    Added previews to the events schema to show images and formatted dates

--- sanity-studio/schemaTypes/event.js
+++ sanity-studio/schemaTypes/event.js
@@ -1,18 +1,23 @@
 import {defineField, defineType} from 'sanity'
+import {CalendarIcon} from '@sanity/icons'
 
 export const eventType = defineType({
   name: 'event',
-  title: 'Event',
+  title: 'Events',
+  icon: CalendarIcon,
   type: 'document',
   fields: [
     defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
+      validation: (rule) => rule.required().error(`Name of event required`),
     }),
     defineField({
       name: 'slug',
       type: 'slug',
+      options: {source: 'name'},
+      validation: (rule) => rule.required().error(`Required to generate a page on the website`),
     }),
     defineField({
       name: 'description',
@@ -39,11 +44,49 @@ export const eventType = defineType({
           {title: 'Film Screening', value: 'film screening'},
           {title: 'Chess Night', value: 'chess'},
         ],
+        layout: 'radio',
       },
     }),
     defineField({
       name: 'tickets',
       type: 'url',
     }),
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      date: 'date',
+      image: 'image',
+    },
+    prepare({name, date, image}) {
+      const nameFormatted = name || 'Untitled event'
+      const dateFormatted = date
+        ? new Date(date).toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })
+        : 'No date'
+
+      return {
+        title: nameFormatted,
+        subtitle: dateFormatted,
+        media: image || CalendarIcon,
+      }
+    },
+  },
+  orderings: [
+    {
+      title: 'Date (Ascending)',
+      name: 'dateAsc',
+      by: [{field: 'date', direction: 'asc'}],
+    },
+    {
+      title: 'Date (Descending)',
+      name: 'dateDesc',
+      by: [{field: 'date', direction: 'desc'}],
+    }
   ],
 })

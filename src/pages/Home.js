a7b268006014be09819b889cf89f5589f96f55e7
Brad Heath <bradheath96@gmail.com>
2025-01-08 13:39:29 +0000

    Added basic layout to refine for the homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+
+const Home = () => (
+	<div
+		className="flex flex-col items-center justify-center h-screen"
+		style={{ backgroundColor: "#F93631" }}>
+		<h1 className="text-4xl font-bold mb-4">Welcome to Mulackei!</h1>
+        <p className="text-lg mb-6">Your hub for live music, art, and more.</p>
+        
+	</div>
+);
+
+export default Home

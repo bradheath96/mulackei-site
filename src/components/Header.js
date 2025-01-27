6e0b92c923de6760a18626bd84b1c0ea5a345bf1
Brad Heath <bradheath96@gmail.com>
2025-01-27 10:44:24 +0000

    Added svg icons to the header, need to sort out tailwind justifying right

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,40 @@
 import { Link } from "react-router-dom";
+import FacebookIcon from "../assets/facebook.svg";
+import InstagramIcon from "../assets/instagram.svg";
 
 const Header = () => (
-	<header className="bg-floralWhite sticky top-0 z-50 p-4 flex justify-between items-center ">
-		<h1 className="text-xl  text-black font-light px-32">
+	<header class="bg-floralWhite sticky top-0 z-50 p-3 flex justify-between items-center">
+		{/*Logo*/}
+		<h1 class="text-xl text-black font-light ml-5">
 			<Link to="/">Mulackei</Link>
 		</h1>
-		<nav className="text-xl text-black font-light px-32">
-			<Link to="/About" className="px-3">
+
+		{/* Navigation Links */}
+		<nav class="flex items-center space-x-5 mr-5 justify-center  p-2">
+			{/* Page Links */}
+			<Link to="/about" class="text-xl text-black font-light">
 				About
 			</Link>
-			<Link to="/events" className="px-3">
+			<Link to="/events" class="text-xl text-black font-light">
 				Events
 			</Link>
-			<Link to="/contact" className="px-3">
+			<Link to="/contact" class="text-xl text-black font-light text-left">
 				Contact
 			</Link>
+
+			{/* Social Media Links */}
+			<a
+				href="https://www.facebook.com/profile.php?id=100092280880824"
+				target="_blank"
+				rel="noopener noreferrer">
+				<img src={FacebookIcon} alt="Facebook" class="w-8 h-8" />
+			</a>
+			<a
+				href="https://www.instagram.com/mulackei_/"
+				target="_blank"
+				rel="noopener noreferrer">
+				<img src={InstagramIcon} alt="Instagram" class="w-8 h-8" />
+			</a>
 		</nav>
 	</header>
 );

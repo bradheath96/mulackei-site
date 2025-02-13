53f785eb64221614222d2383a89718d633cc8402
Brad Heath <bradheath96@gmail.com>
2025-02-13 13:26:52 +0000

    fixed disclosure issues with the dropdown menu, menu now dissapears when a new link is clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,6 @@
-import {
-	Disclosure,
-	DisclosureButton,
-	DisclosurePanel,
-} from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const navigation = [
 	{ name: "About", to: "/about" },
@@ -12,23 +8,24 @@ const navigation = [
 	{ name: "Contact", to: "/contact" },
 ];
 
-function classNames(...classes) {
-	return classes.filter(Boolean).join(" ");
-}
-
 const Header = () => {
+	const location = useLocation();
+	const [isOpen, setIsOpen] = useState(false);
+
+	useEffect(() => {
+		setIsOpen(false);
+	}, [location.pathname]);
+
 	return (
-		<Disclosure as="nav" className="bg-floralWhite sticky top-0 z-[100]">
+		<nav className="bg-floralWhite sticky top-0 z-[100]">
 			<div className="w-full px-6 sm:px-8 lg:px-10">
 				<div className="relative flex h-16 items-center justify-between">
-					{/* Logo Section */}
 					<div className="flex items-center">
 						<Link key="Home" to="/">
 							<h1 className="text-3xl font-logoFont">Mulackei</h1>
 						</Link>
 					</div>
 
-					{/* Desktop Navigation */}
 					<div className="hidden sm:flex ml-auto space-x-2">
 						{navigation.map((item) => (
 							<Link
@@ -40,36 +37,38 @@ const Header = () => {
 						))}
 					</div>
 
-					{/* Mobile Menu Button */}
 					<div className="sm:hidden">
-						<DisclosureButton className="text-black p-2 focus:outline-none">
-							{({ open }) =>
-								open ? (
-									<XMarkIcon aria-hidden="true" className="size-6" />
-								) : (
-									<Bars3Icon aria-hidden="true" className="size-6" />
-								)
-							}
-						</DisclosureButton>
+						<button
+							onClick={() => setIsOpen(!isOpen)}
+							className="text-black p-2 focus:outline-none">
+							{isOpen ? (
+								<XMarkIcon aria-hidden="true" className="size-6" />
+							) : (
+								<Bars3Icon aria-hidden="true" className="size-6" />
+							)}
+						</button>
 					</div>
 				</div>
 			</div>
 
-			{/* Mobile Navigation Panel */}
-			<DisclosurePanel className="sm:hidden">
-				<div className="space-y-2 pb-3 px-36 text-center">
-					{navigation.map((item) => (
-						<Link
-							key={item.name}
-							to={item.to}
-							className="block text-black hover:bg-boxYellow hover:text-black py-2 text-base font-medium">
-							{item.name}
-						</Link>
-					))}
+			{isOpen && (
+				<div className="sm:hidden absolute w-full bg-floralWhite shadow-lg">
+					<div className="space-y-2 pb-3 px-36 text-center">
+						{navigation.map((item) => (
+							<Link
+								key={item.name}
+								to={item.to}
+								onClick={() => setIsOpen(false)} 
+								className="block text-black hover:bg-boxYellow hover:text-black py-2 text-base font-medium">
+								{item.name}
+							</Link>
+						))}
+					</div>
+					<hr className="border-b-[1.5px] border-boxYellow" />
 				</div>
-			</DisclosurePanel>
+			)}
 			<hr className="border-b-[1.5px] border-boxYellow" />
-		</Disclosure>
+		</nav>
 	);
 };
 

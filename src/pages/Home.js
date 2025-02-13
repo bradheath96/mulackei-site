c8bf685dc8af1cf95a826f3200978a5f546623a8
Brad Heath <bradheath96@gmail.com>
2025-02-13 15:17:39 +0000

    Minor changes to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
 	}, []);
 
 	return (
-		<div className="relative">
+		<div className=" relative">
 			{/* Hero Section with Background Image */}
 			<div className="relative min-h-[30vh] overflow-hidden">
 				<div
-					className="absolute inset-0 bg-cover bg-center bg-no-repeat scale-200 transform"
+					className="bg-primary absolute inset-0 bg-cover bg-center bg-no-repeat scale-200 transform"
 					style={{ backgroundImage: `url(${Mulackei})` }}>
 					<div className="absolute inset-0 bg-black/50"></div>{" "}
 					{/* Dark Overlay */}
@@ -19,21 +19,19 @@ const Home = () => {
 
 				{/* Text Overlay */}
 				<section className="relative flex flex-col items-center justify-center min-h-[30vh] text-center p-6">
-					<h1 className="animate-fade-up font-titleFont text-3xl lg:text-6xl font-bold mb-4 text-white">
+					<h1 className="animate-fade animate-duration-1000 font-titleFont text-3xl lg:text-6xl font-bold mb-4 text-white">
 						Welcome to Mulackei
 					</h1>
 				</section>
 			</div>
 
 			{/* About Us Section */}
-			<div className="bg-primary text-white py-10 px-6 text-left">
-				<h2 className="animate-fade-up text-2xl font-bold mb-4">About Us</h2>
-				<p className="animate-fade-up max-w-2xl mx-auto text-md font-bodyFont">
+			<div className="bg-primary text-white py-5 px-6 text-left">
+				<p className="animate-fade animate-duration-1000 max-w-2xl mx-auto text-md font-bodyFont">
 					The Mulackei is a non-profit association dedicated to art, readings,
 					workshops, concerts and other cultural events. Our goal is to create a
 					space for encounters and creative exchange that brings together people
 					from different backgrounds.
-					
 				</p>
 			</div>
 		</div>

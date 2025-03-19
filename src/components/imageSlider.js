afb54d44587953e46769d30c1c29dfaa47fd2bdf
Brad Heath <bradheath96@gmail.com>
2025-03-19 15:32:44 +0000

    started to adjust the slider in the about page

--- src/components/imageSlider.js
+++ src/components/imageSlider.js
@@ -5,13 +5,17 @@ export default function ImageSlider({ imageUrls }) {
 	const [imageIndex, setImageIndex] = useState(0);
 
 	return (
-		<div>
-			<img src={imageUrls[imageIndex]} alt={`Slide ${imageIndex + 1}`} />
-			<button>
-				<ArrowBigLeft />
+		<div className="w-full h-full relative">
+			<img
+				src={imageUrls[imageIndex]}
+				alt={`Slide ${imageIndex + 1}`}
+				className="object-cover w-full h-full block"
+			/>
+			<button className="clear block absolute top-0 bottom-0 p-4 cursor-pointer ">
+				<ArrowBigLeft className="stroke-white fill-black" />
 			</button>
 			<button>
-				<ArrowBigRight />
+				<ArrowBigRight className="stroke-white fill-black" />
 			</button>
 		</div>
 	);

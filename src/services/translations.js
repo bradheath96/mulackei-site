f47683d9eac58a5ec0b32e5e0ff4005e4c26f25a
Brad Heath <bradheath96@gmail.com>
2025-06-04 14:27:51 +0100

    started the translation services by adding translations in an object to fetch later on

---
+++ src/services/translations.js
@@ -0,0 +1,20 @@
+export const translations = {
+	home: {
+		en: {
+			Heading: "Coming Up",
+		},
+		de: {
+			Heading: "Demnächst",
+		},
+	},
+	about: {
+		en: {
+			Heading: "About Us",
+			Teamheading: "Meet the Team",
+		},
+		de: {
+			Heading: "Über uns",
+			Teamheading: "Lernen Sie das Team kennen",
+		},
+	},
+};

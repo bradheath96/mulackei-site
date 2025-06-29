eb6953daec67594b96dd2406c9fa831b6f4c857a
Brad Heath <bradheath96@gmail.com>
2025-06-29 21:07:13 +0100

    added more language features but needs a lot of work to the language toggler

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,21 @@ import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import LanguageToggle from "./LanguageToggle";
 
-const navigation = [
-	{ name: "About", to: "/about" },
-	{ name: "Events", to: "/events" },
-	{ name: "Contact", to: "/contact" },
-];
+// Navigation labels by language
+const navLabels = {
+	en: {
+		About: "About",
+		Events: "Events",
+		Contact: "Contact",
+	},
+	de: {
+		About: "Über uns",
+		Events: "Veranstaltungen",
+		Contact: "Kontakt",
+	},
+};
 
-const Header = () => {
+const Header = ({ currentLang }) => {
 	const location = useLocation();
 	const [isOpen, setIsOpen] = useState(false);
 
@@ -17,30 +25,32 @@ const Header = () => {
 		setIsOpen(false);
 	}, [location.pathname]);
 
+	const navItems = [
+		{ key: "About", path: "about" },
+		{ key: "Events", path: "events" },
+		{ key: "Contact", path: "contact" },
+	];
+
 	return (
 		<nav className="bg-floralWhite sticky top-0 z-[100]">
 			<div className="w-full px-6 sm:px-8 lg:px-10">
 				<div className="relative flex h-16 items-center justify-between">
 					<div className="flex items-center">
-						<Link key="Home" to="/">
+						<Link to={`/${currentLang}`}>
 							<h1 className="text-3xl font-logoFont">Mulackei</h1>
 						</Link>
 					</div>
 
 					<div className="hidden sm:flex ml-auto space-x-2">
-						{navigation.map((item) => (
+						{navItems.map((item) => (
 							<Link
-								key={item.name}
-								to={item.to}
+								key={item.key}
+								to={`/${currentLang}/${item.path}`}
 								className="font-bodyFont text-black hover:bg-boxYellow hover:text-black rounded-sm px-4 py-2 text-lg font-medium transition duration-300 ease-in-out hover:-translate-y-0.5 hover:-translate-x-0.5">
-								{item.name}
+								{navLabels[currentLang][item.key]}
 							</Link>
 						))}
-						<LanguageToggle
-							onToggle={(lang) => {
-								console.log("Language switched to:", lang);
-							}}
-						/>
+						<LanguageToggle currentLang={currentLang} />
 					</div>
 
 					<div className="sm:hidden">
@@ -60,13 +70,13 @@ const Header = () => {
 			{isOpen && (
 				<div className="sm:hidden absolute w-full bg-floralWhite shadow-lg">
 					<div className="space-y-2 pb-3 px-36 text-center">
-						{navigation.map((item) => (
+						{navItems.map((item) => (
 							<Link
-								key={item.name}
-								to={item.to}
+								key={item.key}
+								to={`/${currentLang}/${item.path}`}
 								onClick={() => setIsOpen(false)}
 								className="block text-black hover:bg-boxYellow hover:text-black py-2 text-base font-medium">
-								{item.name}
+								{navLabels[currentLang][item.key]}
 							</Link>
 						))}
 					</div>
